b349403b55385ae52981195b8464dffde0caefdf
Fresh Digital <freshdigital9@gmail.com>
2025-11-20 11:28:38 +0700

    replace head - gemini AI.php

--- crm/application/views/admin/includes/head.php
+++ crm/application/views/admin/includes/head.php
@@ -2,8 +2,7 @@
 <?php $isRTL = (is_rtl() ? 'true' : 'false'); ?>
 
 <!DOCTYPE html>
-<html lang="<?= e($locale); ?>"
-    dir="<?= ($isRTL == 'true') ? 'rtl' : 'ltr' ?>">
+<html lang="<?= e($locale); ?>" dir="<?= ($isRTL == 'true') ? 'rtl' : 'ltr' ?>">
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -15,12 +14,18 @@
     </title>
 
     <?= app_compile_css(); ?>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>
+        // Patch agar simbol $ dan jQuery dikenali global
+        window.jQuery = window.$ = jQuery;
+    </script>
     <?php render_admin_js_variables(); ?>
 
     <script>
-        var totalUnreadNotifications = <?= e($current_user->total_unread_notifications); ?> ,
-            proposalsTemplates = <?= json_encode(get_proposal_templates()); ?> ,
-            contractsTemplates = <?= json_encode(get_contract_templates()); ?> ,
+        var totalUnreadNotifications = <?= e($current_user->total_unread_notifications); ?>,
+            proposalsTemplates = <?= json_encode(get_proposal_templates()); ?>,
+            contractsTemplates = <?= json_encode(get_contract_templates()); ?>,
             billingAndShippingFields = ['billing_street', 'billing_city', 'billing_state', 'billing_zip',
                 'billing_country',
                 'shipping_street', 'shipping_city', 'shipping_state', 'shipping_zip', 'shipping_country'
@@ -30,12 +35,9 @@
             expensePreviewDropzone, taskTrackingChart, cfh_popover_templates = {},
             _table_api;
     </script>
+    
     <?php app_admin_head(); ?>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-  // Kode patch agar $ dikenali sebagai jQuery
-  window.jQuery = window.$ = jQuery;
-</script>
+
 </head>
 
 <body <?= admin_body_class($bodyclass ?? ''); ?>>
