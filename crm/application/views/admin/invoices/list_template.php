6abd12fff31f63f6abee3d0bd4202951d8a21020
Fresh Digital <freshdigital9@gmail.com>
2025-11-20 12:13:34 +0700

    Update list_template.php

--- crm/application/views/admin/invoices/list_template.php
+++ crm/application/views/admin/invoices/list_template.php
@@ -1,60 +1,26 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <div class="col-md-12">
-    <div class="row">
-        <div class="col-md-12 tw-mb-2">
-            <div class="_buttons sm:tw-space-x-1 rtl:sm:tw-space-x-reverse">
-                
+    <div class="panel_s">
+        <div class="panel-body">
+            <div class="_buttons tw-mb-4">
                 <?php if (staff_can('create', 'invoices')) { ?>
-                <a href="<?= admin_url('invoices/invoice'); ?>"
-                    class="btn btn-primary pull-left new new-invoice-list">
-                    <i class="fa-regular fa-plus tw-mr-1"></i>
-                    <?= _l('create_new_invoice'); ?>
-                </a>
-                <?php } ?>
-
-                <?php if (! isset($project) && ! isset($customer)) { ?>
-                    <?php if (staff_can('create', 'payments')) { ?>
-                    <button id="add-batch-payment" onclick="add_batch_payment()" class="btn btn-default pull-left">
-                        <i class="fa-solid fa-plus tw-mr-1"></i>
-                        <?= _l('batch_payments'); ?>
-                    </button>
-                    <?php } ?>
-
-                    <?php if (staff_can('view', 'bulk_pdf_exporter')) { ?>
-                    <a href="<?= admin_url('utilities/bulk_pdf_exporter?feature=invoices'); ?>"
-                        data-toggle="tooltip"
-                        title="<?= _l('bulk_pdf_exporter'); ?>"
-                        class="btn-with-tooltip pull-left btn btn-default !tw-px-3">
-                        <i class="fa-regular fa-file-pdf"></i>
+                    <a href="<?= admin_url('invoices/invoice'); ?>" class="btn btn-primary">
+                        <i class="fa-regular fa-plus tw-mr-1"></i>
+                        <?= _l('create_new_invoice'); ?>
                     </a>
-                    <?php } ?>
                 <?php } ?>
+            </div>
+            
+            <div class="clearfix"></div>
+            <hr class="hr-panel-heading" />
 
-                <div class="display-block pull-right tw-space-x-0 sm:tw-space-x-1.5 rtl:tw-space-x-reverse">
-                    <a href="#" class="btn btn-default btn-with-tooltip toggle-small-view hidden-xs sm:!tw-px-3"
-                        onclick="toggle_small_view('.table-invoices','#invoice'); return false;" data-toggle="tooltip"
-                        title="<?= _l('invoices_toggle_table_tooltip'); ?>"><i
-                            class="fa fa-angle-double-left"></i>
-                    </a>
-                    
-                    </div>
-                <div class="clearfix"></div>
+            <div class="col-md-12" id="small-table">
+                <?php $this->load->view('admin/invoices/table_html'); ?>
             </div>
-        </div>
-        <div class="clearfix"></div>
-        
-        <div class="col-md-12" id="small-table">
-            <div class="panel_s">
-                <div class="panel-body panel-table-full">
-                    <?= form_hidden('invoiceid', $invoiceid); ?>
-                    
-                    <?php $this->load->view('admin/invoices/table_html'); ?>
-                </div>
+
+            <div class="col-md-7 small-table-right-col">
+                <div id="invoice" class="hide"></div>
             </div>
-        </div>
-        
-        <div class="col-md-7 small-table-right-col">
-            <div id="invoice" class="hide"></div>
         </div>
     </div>
 </div>
